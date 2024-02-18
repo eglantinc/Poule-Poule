25bc945be90812936658c1be3993b9eadd23a5e7
Clervil, Eglantine Anne <clervil.eglantine_anne@courrier.uqam.ca>
2024-02-17 23:01:54 -0500

    correction d'erreur pour poules et chiens

--- poule.s
+++ poule.s
@@ -2,19 +2,23 @@
 # Églantine Anne Clervil - CLEE89530109 (groupe 011)
 # Nawal Tatari - TATN21598407 (groupe 011)
 #
-# Ce programme est un jeu inspiré du concept du jeu poule-poule. Les règles du jeu impliquent 
-# l'utilisation de cartes représentées par des caractères (lettres minuscules et chiffres):
-# Une carte œuf (lettre 'o') ajoute un œuf au jeu. Le jeu commence avec aucun œuf.
-# Une carte d'affichage (lettre 'a') affiche le nombre d'œufs disponibles en jeu.
-# Une carte coq (lettre 'q') fait chanter le coq et termine le programme.
-# Chaque lettre peut être préfixée par un multiplicateur (chiffre de 0 à 9) indiquant la quantité de cartes jouées.
-# Une carte poule (lettre 'p') couve un œuf si au moins un œuf est disponible, le rendant indisponible. Sinon, elle ne fait rien.
-# Une carte renard (lettre 'r') chasse une des poules qui couvent un œuf. Si aucune poule n'est en jeu, 
-# le renard passe sans rien faire.
-# Une carte chien (lettre 'c') poursuit un renard s'il apparaît, le faisant fuir. Les autres chiens restent en jeu.
-# Une carte ver (lettre 'v') reste en jeu et est ignorée par les chiens, les renards et les poules. 
-# Cependant, une nouvelle poule le poursuit sans considérer la disponibilité des œufs.
-	
+# Ce programme est un jeu inspiré du concept du jeu poule-poule. 
+# Les règles du jeu impliquent l'utilisation de cartes représentées par des 
+# caractères (lettres minuscules et chiffres). Une carte œuf (lettre 'o')
+# ajoute un œuf au jeu. Le jeu commence avec aucun œuf.
+# Une carte d'affichage (lettre 'a') affiche le nombre d'œufs disponibles.
+# Une carte coq (lettre 'q') termine le programme. Chaque lettre peut être
+# préfixée par un multiplicateur (0 à 9) indiquant la quantité de cartes. 
+# Une carte poule (lettre 'p') couve un œuf le rendant indisponible. 
+# Sinon, elle ne fait rien. Une carte renard (lettre 'r') chasse une des poules
+# qui couvent un œuf. Si aucune poule n'est en jeu, le renard fait rien.
+# Une carte chien (lettre 'c') poursuit un renard s'il apparaît. Les autres
+# chiens restent en jeu. Une carte ver (lettre 'v') reste en jeu et est 
+# ignorée par les chiens, les renards et les poules. Une nouvelle poule 
+# le poursuit sans considérer la disponibilité des œufs. Si un caractère
+# autre que ceux mentionnés est entré, le programme affiche 'Err' et se termine.
+# Le code Extra: grand multiplicateur n'a pas été implémenté.
+
 	.eqv PrintInt, 1
 	.eqv PrintChar, 11
 	.eqv ReadChar, 12
@@ -24,20 +28,21 @@
 	li s1, 0			# compteur de poules
 	li s2, 0			# compteur de chiens
 	li s3, 0			# compteur de vers	
-	li t1, 1			# Initialiser le chiffre courant a 1,
-					# pour gerer les cas multiple et les cas non multiple
-  	li s4,  1			# Variable pour reinitialiser t1
+	
+	# Initialiser le chiffre courant à 1, pour gérer les cas multiple et non multiple
+	li t1, 1
+  	li s4, 1			# Variable pour réinitialiser t1
 
 loop:
 	li a7, ReadChar
 	ecall
  	
 	li t0, 'q' 
-	beq a0, t0, fin			# Si le caractere lu est 'q', le jeu se termine		
+	beq a0, t0, fin			# Si le caractère lu est 'q', le jeu se termine		
 
 	li t0, 'a'
-	bne a0, t0, case_multip	# Si le caractere lu est 'a', affiche
-	beqz t1, loop  # Si dans le cas multiple aucun affichage n'est demande
+	bne a0, t0, case_multip		# Si le caractère lu est 'a', affiche
+	beqz t1, loop			# Si dans le cas multiple aucun affichage n'est demandé
 	j affiche_oeuf
 	
 case_multip:
@@ -52,20 +57,20 @@ case_multip:
 
 case_oeuf:
 	li t0, 'o'
-	bne t0, a0, case_poule		# Si a0 != 'o', verifier le prochain caractere
+	bne t0, a0, case_poule		# Si a0 != 'o', verifier le prochain caractère
 	j ajouter_oeuf
 
 case_poule:
 	li t0, 'p'
-	bne t0, a0, case_renard		# Si a0 != 'p', verifier le prochain caractere
+	bne t0, a0, case_renard		# Si a0 != 'p', verifier le prochain caractère
 	bgtz s3, diminuer_ver
 	bgtz s0, diminuer_oeuf
-	mv t1, s4			# Reinitialiser t1  a 1
+	mv t1, s4			# Réinitialiser t1 a 1
 	j loop
 	
 case_renard:
 	li t0 'r'
-	bne t0, a0, case_chien		# Si a0 != 'r', verifier le prochain caractere
+	bne t0, a0, case_chien		# Si a0 != 'r', verifier le prochain caractère
 	bgtz s2, diminuer_chien
 	bgtz s1, diminuer_poule		# S'il y a des poules couvrant des oeufs
 	mv t1, s4
@@ -73,7 +78,7 @@ case_renard:
 
 case_chien:
 	li t0, 'c'
-	bne t0, a0, case_ver		# Si a0 != 'c', verifier le prochain caractere	
+	bne t0, a0, case_ver		# Si a0 != 'c', verifier le prochain caractère	
 	add s2, s2, t1
 	mv t1, s4
 	j loop
@@ -81,24 +86,24 @@ case_chien:
 case_ver:
 	li t0, 'v'
 	bne t0, a0, case_erreur		# Si a0 != 'v', affiche erreur
-	add s3, s3, t1			# Incrementer le nombre de cartes de ver au compteur de ver
+	add s3, s3, t1			# Incrémenter le nombre de cartes de ver au compteur de vers
 	mv t1, s4
 	j loop
 
 ajouter_oeuf:
-	add s0, s0, t1			# Incrementer le nombre d'oeufs
+	add s0, s0, t1			# Incrémenter le nombre d'oeufs
 	bltz s1, reinitialiser_poule 
-	mv t1, s4			# Reinitialiser le chiffre courant a 1
+	mv t1, s4			# Réinitialiser le chiffre courant a 1
 	j loop   
 
 ajouter_poule:
-	add s1, s1, t1			# Incrementer le nombre de poules
-	bltz s0, reinitialiser_oeuf	# Si le nombre d'oeuf est negatif, reinitialiser oeuf
+	add s1, s1, t1			# Incrémenter le nombre de poules
+	bltz s0, reinitialiser_oeuf	# Si le nombre d'oeufs est négatif, réinitialiser oeuf
 	mv t1, s4
 	j loop
 	
 diminuer_oeuf:
-	sub s0, s0, t1			# Diminuer le nombre d'oeuf avec le chiffre courant
+	sub s0, s0, t1			# Diminuer le nombre d'oeufs avec le chiffre courant
 	j ajouter_poule
 
 diminuer_poule:
@@ -118,37 +123,37 @@ diminuer_ver:
 	j loop  
 
 reinitialiser_oeuf:
-	add s1, s0, s1			# Tenir compte seul des poules qui couvrent des oeufs
+	add s1, s0, s1			# Tenir compte seulement des poules qui couvrent des oeufs
 	sub s0, s0, s0
 	mv t1, s4
 	j loop
 
 reinitialiser_poule:
-	add s0, s0, s1			# Ajouter la valeur (negative) du nombre de poules aux oeufs
-					# La valeur courante de chiffre ne nous interesse pas
+	# Ajouter la valeur (négative) du nombre de poules aux oeufs
+	add s0, s0, s1			
 	sub s1, s1, s1
 	mv t1, s4
 	j loop
 
 reinitialiser_chien:
-	add s1, s1, s2 		# A ce stade, la valeur de chien est negative, puisqu'il y a plus de renard courant que de chien
+	add s1, s1, s2 		
 	sub s0, s0, s2		
 	sub s2, s2, s2
+	bltz s1, diminuer_poule
 	mv t1, s4
 	j loop
 	
 reinitialiser_ver:
-	add s0, s0, s3			# Si le nombre de ver est negatif, on additionne aux oeuf
-	sub s1, s1, s3 
+	add s0, s0, s3			# Ajuster le nombre d'oeufs
+	sub s1, s1, s3 			# Le nombre de poules courant
 	sub s3, s3, s3
+	bltz s0, diminuer_oeuf		# Si le nombre d'oeuf est negatif
 	mv t1, s4
 	j loop
 
-	
+ 					
 case_erreur:
 	li a7, PrintChar
-	li a0, '\n'
-	ecall
 	li a0, 'E'
 	ecall
 	li a0, 'r'
@@ -158,8 +163,7 @@ case_erreur:
 	j fin
 
 affiche_oeuf:
-	addi t1, t1, -1 # Decrementer le t1
-	
+	addi t1, t1, -1			# Décrementer le t1
 	li a7, PrintInt
 	mv a0, s0
 	ecall
@@ -168,12 +172,9 @@ affiche_oeuf:
 	li a0, '\n'
 	ecall
 	
-	bgtz t1, affiche_oeuf # Tant qu'on a besoin d'afficher les oeufs
-	
+	bgtz t1, affiche_oeuf		# Tant qu'on a besoin d'afficher les oeufs
 	mv t1, s4
 	j loop
-
- 
 
 fin:	li a7, Exit
 	ecall
